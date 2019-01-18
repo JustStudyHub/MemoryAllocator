4e04564254533f64acf31b7dcca89b6b868b245b
OAI <ivanova7elena@gmail.com>
2019-01-18 14:03:22 +0200

    Add clock benchmark Alloc buffer size in heap

--- MemoryAllocator/MemoryAllocator.cpp
+++ MemoryAllocator/MemoryAllocator.cpp
@@ -1,5 +1,7 @@
 
+
 #include "pch.h"
+#include "time.h"
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -26,12 +28,13 @@ private:
 		size_t m_dSize;		
 		size_t m_dStart;
 	};
-	static const size_t m_numOfObj = 10000;
+	static const size_t m_numOfObj = 1000;
 	static const size_t m_infDataSize = sizeof(DataInf);
 	static const size_t m_objSize = sizeof(T);
 	static const size_t m_objDataShift = m_numOfObj * m_infDataSize;
 	static const size_t m_objDataSize = m_numOfObj * m_objSize;
-	char m_buffer[m_objDataShift + m_objDataSize];
+	//char m_buffer[m_objDataShift + m_objDataSize];
+	char* m_buffer = new char[m_objDataShift + m_objDataSize];
 
 	std::stack<DataInf*> freeMemoryStack;
 	DataInf* m_startInfBufer;
@@ -75,17 +78,6 @@ T* MemoryAlloc<T>::Add(T obj)
 		tempInfPtr->m_dSize = sizeof(obj);
 		freeMemoryStack.pop();
 	}
-	/*for (size_t i = 0; i < m_numOfObj; i++)
-	{
-		tempInfPtr = m_startInfBufer + i;
-		if (tempInfPtr->m_isMemoryFree)
-		{
-			tempObjPtr = reinterpret_cast<T*>(new(m_buffer + tempInfPtr->m_dStart) T(obj));
-			tempInfPtr->m_isMemoryFree = false;
-			tempInfPtr->m_dSize = sizeof(obj);
-			break;
-		}			
-	}*/
 	return tempObjPtr;
 }
 
@@ -102,14 +94,59 @@ T* MemoryAlloc<T>::Delete(T* obj)
 using namespace std;
 int main()
 {
-	//MemoryAlloc<string> t1;
+	
+	stack<string*> test1;
+	stack<string*> test2;
+	stack<string*> test3;
 	/*string* p1 = t1.Add("fytu");
 	string* p2 = t1.Add("q");
 	string* p3 = t1.Add("qwerjklqwerjklqwerjklqwerjklqwerjklqwerjklqwerjkl");*/
-	//cout << *t1.Add("fytu") << endl;
-	//cout << *t1.Add("q") << endl;
-	//cout << *t1.Add("qwerjklqwerjklqwerjklqwerjklqwerjklqwerjklqwerjkl") << endl;
-	MemoryAlloc<int> t1;
+	string temp = "";
+	/*for (int i = 0; i < 1000000; ++i)
+	{
+		test3.push_back(new string("qwertyqwerty"));
+	}*/
+	MemoryAlloc<string> t1;
+	MemoryAlloc<string> t2;
+	MemoryAlloc<string> t3;
+	MemoryAlloc<string> t4;
+	MemoryAlloc<string> t5;
+	MemoryAlloc<string> t6;
+	MemoryAlloc<string> t7;
+	MemoryAlloc<string> t8;
+	MemoryAlloc<string> t9;
+	MemoryAlloc<string> t10;
+	clock_t t = clock();
+	
+	for (int i = 0; i < 1000; ++i)
+	{
+		test1.push(t1.Add("qwertyqwerty"));
+		test1.push(t2.Add("qwertyqwerty"));
+		test1.push(t3.Add("qwertyqwerty"));
+		test1.push(t4.Add("qwertyqwerty"));
+		test1.push(t5.Add("qwertyqwerty"));
+		test1.push(t6.Add("qwertyqwerty"));
+		test1.push(t7.Add("qwertyqwerty"));
+		test1.push(t8.Add("qwertyqwerty"));
+		test1.push(t9.Add("qwertyqwerty"));
+		test1.push(t10.Add("qwertyqwerty"));
+	}
+	t = clock() - t;
+	//t = float(t) / CLOCKS_PER_SEC * 1000;
+
+	cout << t << "    ";
+
+	clock_t t0 = clock();
+	for (int i = 0; i < 10000; ++i)
+	{
+		test2.push(new string("qwertyqwerty"));
+	}
+	t0 = clock() - t0;
+	//t2 = float(t2) / CLOCKS_PER_SEC * 1000;
+	cout << t0;
+
+	
+	/*MemoryAlloc<int> t1;
 	vector<int*> test;
 	for (int i = 0; i < 10000; ++i)
 	{
@@ -127,7 +164,7 @@ int main()
 	else
 	{
 		cout << "nullptr" << endl;
-	}
+	}*/
 	
 	//int* p1 = t1.Add(100);
 	//int* p2 = t1.Add(200);
