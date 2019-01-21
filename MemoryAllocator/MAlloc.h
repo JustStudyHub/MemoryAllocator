073794278dd4e86bad4cf5bc76c2f193830df26b
OAI <ivanova7elena@gmail.com>
2019-01-21 12:52:57 +0200

    Add TestProject (no tests), fix Delete metod

--- MemoryAllocator/MAlloc.h
+++ MemoryAllocator/MAlloc.h
@@ -3,9 +3,9 @@ template<typename T>
 class MAlloc
 {
 public:
-	MAlloc();
+	MAlloc(size_t numOfObj);
 	~MAlloc();
-	T& Add(T obj);
+	T* Add(T obj);
 	T* Delete(T* obj);
 private:
 	struct DataInf
@@ -21,19 +21,24 @@ private:
 		DataInf* m_prevfreePos;
 		bool m_isMemoryFree;
 	};
-	const size_t m_numOfObj = 100000;
+	size_t m_numOfObj;
 	const size_t m_infDataSize = sizeof(DataInf);
 	const size_t m_objSize = sizeof(T);
-	const size_t m_objDataShift = m_numOfObj * m_infDataSize;
-	const size_t m_objDataSize = m_numOfObj * m_objSize;
-	char* m_buffer = new char[m_objDataShift + m_objDataSize];
+	size_t m_objDataShift;
+	size_t m_objDataSize;
+	char* m_buffer;
 	DataInf* m_startInfBufer;
 	DataInf* m_lastFreePos;
 };
 
 template<typename T>
-MAlloc<T>::MAlloc()
+MAlloc<T>::MAlloc(size_t numOfObj)
 {
+	m_numOfObj = numOfObj;
+	m_objDataShift = m_numOfObj * m_infDataSize;
+	m_objDataSize = m_numOfObj * m_objSize;
+	m_buffer = new char[m_objDataShift + m_objDataSize];
+
 	m_startInfBufer = new(m_buffer) DataInf(true, m_objDataShift, nullptr);
 	m_lastFreePos = m_startInfBufer;
 	DataInf* tempPtr = nullptr;
@@ -56,7 +61,7 @@ MAlloc<T>::~MAlloc()
 }
 
 template<typename T>
-T& MAlloc<T>::Add(T obj)
+T* MAlloc<T>::Add(T obj)
 {
 	T* tempObjPtr = nullptr;
 	if (m_lastFreePos != nullptr)
@@ -65,15 +70,15 @@ T& MAlloc<T>::Add(T obj)
 		m_lastFreePos->m_isMemoryFree = false;
 		m_lastFreePos = m_lastFreePos->m_prevfreePos;
 	}
-	return *tempObjPtr;
+	return tempObjPtr;
 }
-
 template<typename T>
 T* MAlloc<T>::Delete(T* obj)
 {
 	size_t tempAdr = reinterpret_cast<size_t>(obj);
+	size_t tempBufAdr = reinterpret_cast<size_t>(m_buffer);
 	DataInf* tempInfPtr = m_lastFreePos;
-	m_lastFreePos = reinterpret_cast<DataInf*> (tempAdr - m_objDataShift);
+	m_lastFreePos = reinterpret_cast<DataInf*> ((tempAdr - m_objDataShift - tempBufAdr)/m_objSize * m_infDataSize + tempBufAdr);
 	obj->~T();
 	m_lastFreePos->m_isMemoryFree = true;
 	m_lastFreePos->m_prevfreePos = tempInfPtr;
--- MemoryAllocator/MemoryAllocator.cpp
+++ MemoryAllocator/MemoryAllocator.cpp
@@ -2,28 +2,29 @@
 
 int main()
 {
-	std::vector<int> test1;
-	std::vector<int*> test2;
+	std::vector<int*> test1;
+	size_t memorySize = 10000000;
 
-	/*MAlloc<int> t1;
-	for (int i = 0; i < 10000; ++i)
+	/*MAlloc<int> t1(memorySize);
+	for (int i = 0; i < memorySize; ++i)
 	{
 		test1.push_back(t1.Add(i));
 	}
-	std::cout << test1[999] << "\t"<< test1[999] << std::endl;
-	t1.Delete(&test1[999]);
-	std::cout << t1.Add(500) << endl;
-	std::cout << test1[999] << "\t" << test1[999] << std::endl;*/
-
+	std::cout << test1[9] << "\t"<< *test1[9] << std::endl;
+	t1.Delete(test1[9]);
+	std::cout << t1.Add(500) << std::endl;
+	std::cout << test1[9] << "\t" << *test1[9] << std::endl;*/
+	
 	clock_t t0 = clock();
-	for (int i = 0; i < 100000; ++i)
+	for (size_t i = 0; i < memorySize; ++i)
 	{
-		new int(i * 100);
+		new int(i);
 	}
 	t0 = clock() - t0;
-	std::cout << "Defaul memory allocator: \t" << t0 << std::endl;
+	std::cout << "Default memory allocator: \t" << t0 << std::endl;
 	clock_t t = clock();
-	MAlloc<int> t1;/*
+	
+	MAlloc<int> t1(memorySize);/*
 	MAlloc<int> t2;
 	MAlloc<int> t3;
 	MAlloc<int> t4;
@@ -48,7 +49,7 @@ int main()
 		t9.Add(i * 100);
 		t10.Add(i * 100);
 	}*/
-	for (int i = 0; i < 100000; ++i)
+	for (size_t i = 0; i < memorySize; ++i)
 	{
 		t1.Add(i);
 	}
